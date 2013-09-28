2f26d306cee420493f88c6548c8c8e8175828a16
italomg <italogmoraes@gmail.com>
2013-09-28 09:42:46 -0400

    facebook button ON

---
+++ src/hackathon/bbh/MainLoginFragment.java
@@ -0,0 +1,20 @@
+package hackathon.bbh;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainLoginFragment extends Fragment{
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, 
+	        ViewGroup container, 
+	        Bundle savedInstanceState) {
+	    View view = inflater.inflate(R.layout.activity_login, container, false);
+
+	    return view;
+	}
+
+}
