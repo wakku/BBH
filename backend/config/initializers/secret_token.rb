14ec186496e35a30d127ba58322799d8ec5fe024
Fernando Gorodscy <fefow@me.com>
2013-09-28 09:22:19 -0400

    removed old

--- backend/config/initializers/secret_token.rb
+++ backend/config/initializers/secret_token.rb
@@ -16,4 +16,4 @@
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-Backend::Application.config.secret_token = '68928a7252ace3ba1307f65625a1121e7da659192d0b9816dc3c9e564d6ffa65a5cfa293bd46425523fd83ff9870f9a0163fee4403705114cbdf42c94ec6a8cb'
+ChangeTracker::Application.config.secret_token = '4e5ec69edd75dd5878869f9795ab8b0f88bf4cb36ce9ea7fdcb3ed161a4a9e136a57a53d3fad3c8ddad314b1c92bbc5154116c6701929406a01216a0a3f0d232'
