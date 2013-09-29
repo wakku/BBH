60168050194728ecd4fac3e879a70f24a5aff2fd
Fernando Gorodscy <fefow@me.com>
2013-09-29 05:01:16 -0400

    First stage done

--- src/hackathon/bbh/MainActivity.java
+++ src/hackathon/bbh/MainActivity.java
@@ -1,30 +1,14 @@
 package hackathon.bbh;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.DownloadManager.Request;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.util.Log;
+import android.service.textservice.SpellCheckerService.Session;
 import android.view.Menu;
-import com.facebook.*;
-import com.facebook.model.*;
-import android.widget.TextView;
-import android.content.Intent;
 import android.view.View;
 
 
